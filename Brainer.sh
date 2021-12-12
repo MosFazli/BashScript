3310a8a04f9057b0b77a4919dde298dfcf65b28b
MosFazli <MosFazli@gmail.com>
2021-12-12 16:07:40 +0330

    some little more

--- Brainer.sh
+++ Brainer.sh
@@ -5,3 +5,21 @@ echo Hello, Welcome to this game '\n'
 echo Select a game mode: '\n'
 echo 1. Calculation '\n'
 
+read modeSelect
+
+if [ $modeSelect -eq 1 ]
+    then
+        echo Welcome to Calculation game '\n'
+        echo "In this game you should to Calculate the questions given at the given time" '\n'
+        echo "Are you ready ? Y for Yes and N for No" '\n'
+        read readyTest
+        if [ $readyTest -eq 'Y' ]
+            then
+            echo "Let's Go"
+        else
+            goto 
+
+        
+
+
+fi
